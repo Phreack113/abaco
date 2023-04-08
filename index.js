b37398acca7c6ac58824204ecc2227fe6afd5898
Sebastian Talamona <SebastianTalamona@natura.net>
2023-04-07 22:45:43 -0300

    Mejora en botones sostenidos

--- index.js
+++ index.js
@@ -3,13 +3,30 @@ const menos = document.getElementById('menos')
 const back = document.querySelector('.back')
 back.style.left = '10px'
 
-mas.addEventListener('click', () => {
-    const px = parseInt(back.style.left.replace('px',''),10)+1
-    back.style.left = `${px}px`
-})
+mas.addEventListener('contextmenu', function(event) {
+    event.preventDefault();
+});
 
+menos.addEventListener('contextmenu', function(event) {
+    event.preventDefault();
+});
 
-menos.addEventListener('click', () => {
-    const px = parseInt(back.style.left.replace('px',''),10)-1
-    back.style.left = `${px}px`
-})
+const move = (n, el) => {
+    const stopMove = () => {
+        clearInterval(intervalId)
+    }
+
+    el.addEventListener('mouseup', stopMove)
+    el.addEventListener('touchend', stopMove)
+    
+    const intervalId = setInterval(() => {
+        const px = parseInt(back.style.left.replace('px', ''), 10) + (1*n)
+        back.style.left = `${px}px`
+    }, 50)
+}
+
+mas.addEventListener('mousedown', ()=>{move(1, mas)})
+mas.addEventListener('touchstart', ()=>{move(1, mas)})
+
+menos.addEventListener('mousedown', ()=>{move(-1, menos)})
+menos.addEventListener('touchstart', ()=>{move(-1, menos)})
